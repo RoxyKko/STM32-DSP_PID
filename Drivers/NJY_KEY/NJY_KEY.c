d71faef08eb7830a2ca50faecc98898180fb4040
RoxyKko <86786320+RoxyKko@users.noreply.github.com>
2022-05-08 00:15:16 +0800

    Add files via upload

---
+++ Drivers/NJY_KEY/NJY_KEY.c
@@ -0,0 +1,110 @@
+#include "NJY_KEY.h"
+#include "main.h"
+#include "base.h"
+
+uint8_t Key_row[1]={0xff};  							 //保存按键行扫描情况的状态数组
+
+uint8_t num=0;
+uint8_t key_scan()
+{
+	uint8_t i,keynum=0;
+	if(KEY_ROW0_INPUT_Read == GPIO_PIN_SET)						//行1被按下
+	{
+		delay_ms(100);									//消抖
+		if(KEY_ROW0_INPUT_Read == GPIO_PIN_SET)					//行1被按下
+			{
+				for(i = 1;i <= 4;i++)
+				{
+					if(i == 1)	     KEY_CLO0_OUT_LOW;	//逐个拉低每列电平
+					else if(i == 2)	 KEY_CLO1_OUT_LOW;
+					else if(i == 3)	 KEY_CLO2_OUT_LOW;
+					else if(i == 4)	 KEY_CLO3_OUT_LOW;
+
+					if(KEY_ROW0_INPUT_Read == GPIO_PIN_RESET)
+					{
+						keynum = i;
+						if(i == 1)	     KEY_CLO0_OUT_HIGH;	//逐个拉高每列电平
+						else if(i == 2)	 KEY_CLO1_OUT_HIGH;
+						else if(i == 3)	 KEY_CLO2_OUT_HIGH;
+						else if(i == 4)	 KEY_CLO3_OUT_HIGH;
+						break;
+					}
+				}
+			}
+		else if(KEY_ROW1_INPUT_Read == GPIO_PIN_SET)						//行2被按下
+		{
+			delay_ms(100);									//消抖
+			if(KEY_ROW1_INPUT_Read == GPIO_PIN_SET)					//行1被按下
+				{
+					for(i = 1;i <= 4;i++)
+					{
+						if(i == 1)	     KEY_CLO0_OUT_LOW;	//逐个拉低每列电平
+						else if(i == 2)	 KEY_CLO1_OUT_LOW;
+						else if(i == 3)	 KEY_CLO2_OUT_LOW;
+						else if(i == 4)	 KEY_CLO3_OUT_LOW;
+
+						if(KEY_ROW1_INPUT_Read == GPIO_PIN_RESET)
+						{
+							keynum = i;
+							if(i == 1)	     KEY_CLO0_OUT_HIGH;	//逐个拉高每列电平
+							else if(i == 2)	 KEY_CLO1_OUT_HIGH;
+							else if(i == 3)	 KEY_CLO2_OUT_HIGH;
+							else if(i == 4)	 KEY_CLO3_OUT_HIGH;
+							break;
+						}
+					}
+				}
+		}
+		else if(KEY_ROW2_INPUT_Read == GPIO_PIN_SET)						//行3被按下
+				{
+					delay_ms(100);									//消抖
+					if(KEY_ROW2_INPUT_Read == GPIO_PIN_SET)					//行1被按下
+						{
+							for(i = 1;i <= 4;i++)
+							{
+								if(i == 1)	     KEY_CLO0_OUT_LOW;	//逐个拉低每列电平
+								else if(i == 2)	 KEY_CLO1_OUT_LOW;
+								else if(i == 3)	 KEY_CLO2_OUT_LOW;
+								else if(i == 4)	 KEY_CLO3_OUT_LOW;
+
+								if(KEY_ROW2_INPUT_Read == GPIO_PIN_RESET)
+								{
+									keynum = i;
+									if(i == 1)	     KEY_CLO0_OUT_HIGH;	//逐个拉高每列电平
+									else if(i == 2)	 KEY_CLO1_OUT_HIGH;
+									else if(i == 3)	 KEY_CLO2_OUT_HIGH;
+									else if(i == 4)	 KEY_CLO3_OUT_HIGH;
+									break;
+								}
+							}
+						}
+				}
+		else if(KEY_ROW3_INPUT_Read == GPIO_PIN_SET)						//行4被按下
+				{
+					delay_ms(100);									//消抖
+					if(KEY_ROW3_INPUT_Read == GPIO_PIN_SET)					//行1被按下
+						{
+							for(i = 1;i <= 4;i++)
+							{
+								if(i == 1)	     KEY_CLO0_OUT_LOW;	//逐个拉低每列电平
+								else if(i == 2)	 KEY_CLO1_OUT_LOW;
+								else if(i == 3)	 KEY_CLO2_OUT_LOW;
+								else if(i == 4)	 KEY_CLO3_OUT_LOW;
+
+								if(KEY_ROW3_INPUT_Read == GPIO_PIN_RESET)
+								{
+									keynum = i;
+									if(i == 1)	     KEY_CLO0_OUT_HIGH;	//逐个拉高每列电平
+									else if(i == 2)	 KEY_CLO1_OUT_HIGH;
+									else if(i == 3)	 KEY_CLO2_OUT_HIGH;
+									else if(i == 4)	 KEY_CLO3_OUT_HIGH;
+									break;
+								}
+							}
+						}
+				}
+		num=keynum;
+		return keynum;
+
+	}
+}
