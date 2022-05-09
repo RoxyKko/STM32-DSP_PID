9089cd76ecc0b6d1357f8bd39a5af9b0ce263e85
RoxyKko <86786320+RoxyKko@users.noreply.github.com>
2022-05-09 21:35:19 +0800

    Add files via upload

--- 调试/Core/Src/main.d
+++ 调试/Core/Src/main.d
@@ -27,15 +27,17 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- ../Core/Inc/adc.h ../Core/Inc/main.h ../Core/Inc/dma.h ../Core/Inc/tim.h \
- ../Core/Inc/usart.h ../Core/Inc/gpio.h \
- D:/cubeIDE/oled_v1.2/Drivers/base/base.h \
- D:/cubeIDE/oled_v1.2/Drivers/LCD12864/lcd12864.h \
- D:/cubeIDE/oled_v1.2/Drivers/DSP/arm_math.h \
- D:/cubeIDE/oled_v1.2/Drivers/NJY_KEY/NJY_KEY.h
+ ../Core/Inc/adc.h ../Core/Inc/main.h ../Core/Inc/dma.h ../Core/Inc/spi.h \
+ ../Core/Inc/tim.h ../Core/Inc/usart.h ../Core/Inc/gpio.h \
+ D:/cubeIDE/oled_v1.4/Drivers/base/base.h \
+ D:/cubeIDE/oled_v1.4/Drivers/LCD12864/lcd12864.h \
+ D:/cubeIDE/oled_v1.4/Drivers/DSP/arm_math.h \
+ D:/cubeIDE/oled_v1.4/Drivers/NJY_KEY/NJY_KEY.h \
+ D:/cubeIDE/oled_v1.4/Drivers/W25QXX/w25qxx.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -65,16 +67,19 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
 ../Core/Inc/adc.h:
 ../Core/Inc/main.h:
 ../Core/Inc/dma.h:
+../Core/Inc/spi.h:
 ../Core/Inc/tim.h:
 ../Core/Inc/usart.h:
 ../Core/Inc/gpio.h:
-D:/cubeIDE/oled_v1.2/Drivers/base/base.h:
-D:/cubeIDE/oled_v1.2/Drivers/LCD12864/lcd12864.h:
-D:/cubeIDE/oled_v1.2/Drivers/DSP/arm_math.h:
-D:/cubeIDE/oled_v1.2/Drivers/NJY_KEY/NJY_KEY.h:
+D:/cubeIDE/oled_v1.4/Drivers/base/base.h:
+D:/cubeIDE/oled_v1.4/Drivers/LCD12864/lcd12864.h:
+D:/cubeIDE/oled_v1.4/Drivers/DSP/arm_math.h:
+D:/cubeIDE/oled_v1.4/Drivers/NJY_KEY/NJY_KEY.h:
+D:/cubeIDE/oled_v1.4/Drivers/W25QXX/w25qxx.h:
